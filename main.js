5a21d4395f935358fdaa6b05d6481505319a9f62
ISO53 <102249575+ISO53@users.noreply.github.com>
2023-11-14 15:48:52 +0300

    file and folder import mechanism created

--- main.js
+++ main.js
@@ -1,42 +1,140 @@
-const { app, BrowserWindow, Menu } = require("electron");
-const { dialog } = require("electron");
+// ******************** Declare Variables ********************
+const {app, BrowserWindow, Menu, ipcMain, dialog} = require("electron");
+const fs = require("fs");
+const path = require("path");
 
-app.whenReady().then(() => {
-    createWindow();
-});
+let win;
 
-const createWindow = () => {
-    const win = new BrowserWindow({
+// ************************ JS Starts ************************
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => process.platform !== "darwin" && app.quit());
+getMessageFromRenderer("open-file-system", openFileSystem);
+
+// ******************** Declare Functions ********************
+function createWindow() {
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         autoHideMenuBar: false,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
+            devTools: true,
             contentSecurityPolicy: "script-src 'self' 'unsafe-inline';",
         },
     });
+
     win.loadFile("index.html");
-    //win.webContents.openDevTools();
     win.maximize();
 
     const menu = Menu.buildFromTemplate([
         {
             label: "File",
-            submenu: [
-                { role: "quit" },
-                { label: "Import Movies", click: () => mainWindow.webContents.executeJavaScript('showImportMoviesPopup();') },
-            ],
+            submenu: [{role: "quit"}, {label: "Import Movies", click: () => sendMessageToRenderer("open-import-movies-popup")}],
         },
-		{
+        {
             label: "Settings",
-            submenu: [
-                { label: "OMDB Api", click: () => console.log("Item 1 clicked") },
-            ],
+            submenu: [{label: "OMDB Api", click: () => sendMessageToRenderer("open-omdb-api-popup")}],
         },
     ]);
 
     Menu.setApplicationMenu(menu);
-};
+}
 
-app.on("window-all-closed", () => process.platform !== "darwin" && app.quit());
+function sendMessageToRenderer(channel, message) {
+    if (win && !win.isDestroyed()) {
+        win.webContents.send(channel, message);
+    }
+}
+
+function getMessageFromRenderer(channel, handler) {
+    ipcMain.on(channel, (event, arg) => {
+        handler(arg);
+    });
+}
+
+function openFileSystem(arg) {
+    if (arg === "dir") {
+        dialog
+            .showOpenDialog(win, {
+                title: "Select Folder",
+                properties: ["openDirectory"],
+            })
+            .then((result) => {
+                if (!result.canceled) {
+                    const selectedFolder = result.filePaths[0];
+                    const movies = getFolderNames(selectedFolder);
+                    writeFoldersToJson(movies);
+                }
+            })
+            .catch(console.error);
+    } else {
+        dialog
+            .showOpenDialog(win, {
+                title: "Open CSV File",
+                filters: [{name: "CSV Files", extensions: ["csv"]}],
+                properties: ["openFile"],
+            })
+            .then((result) => {
+                if (!result.canceled) {
+                    const selectedCsvFile = result.filePaths[0];
+
+                    readAndParseCsv(selectedCsvFile)
+                        .then(writeFoldersToJson)
+                        .catch(console.error);
+                }
+            })
+            .catch(console.error);
+    }
+
+    console.log(movies);
+}
+
+function getFolderNames(folderPath) {
+    const folderNames = [];
+
+    function traverseFolder(currentPath) {
+        const files = fs.readdirSync(currentPath);
+
+        files.forEach((file) => {
+            const filePath = path.join(currentPath, file);
+
+            if (fs.statSync(filePath).isDirectory()) {
+                folderNames.push(file);
+                traverseFolder(filePath);
+            }
+        });
+    }
+
+    traverseFolder(folderPath);
+    return folderNames;
+}
+
+function readAndParseCsv(filePath) {
+    const csvData = fs.readFileSync(filePath, "utf-8");
+
+    return new Promise((resolve, reject) => {
+        parse(
+            csvData,
+            {
+                columns: true,
+                skip_empty_lines: true,
+            },
+            (err, records) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(records);
+                }
+            }
+        );
+    });
+}
+
+function writeFoldersToJson(folderNames) {
+    const jsonFilePath = path.join(__dirname, "db.json");
+    const folderObjects = folderNames.map((folderName) => ({folderName}));
+
+    fs.writeFileSync(jsonFilePath, JSON.stringify(folderObjects, null, 2), "utf-8");
+    console.log("Folders written to JSON file.");
+}
--- renderer.js
+++ renderer.js
@@ -1,8 +1,51 @@
 // ******************** Declare Variables ********************
+const { ipcRenderer } = require("electron");
 
 // ************************ JS Starts ************************
+getMessageFromMain("open-import-movies-popup", openImportMoviesPopup);
+getMessageFromMain("open-omdb-api-popup", openOmdbApiPopup);
+popupCloseButtonListener();
+importFromFileSystemButtonListener();
+importFromCSVButtonListener();
 
 // ******************** Declare Functions ********************
-function showImportMoviesPopup() {
-    
+function sendMessageToMain(channel, message) {
+    ipcRenderer.send(channel, message);
+}
+
+function getMessageFromMain(channel, handler) {
+    ipcRenderer.on(channel, (event, arg) => {
+        handler(arg);
+    });
+}
+
+function openImportMoviesPopup() {
+    document.getElementById("import_movies_div").classList.remove("hide_popup");
+    document.getElementById("blur_background").classList.add("blur");
+}
+
+function openOmdbApiPopup() {
+    document.getElementById("omdb_api_div").classList.remove("hide_popup");
+    document.getElementById("blur_background").classList.add("blur");
+}
+
+function popupCloseButtonListener() {
+    document.querySelectorAll('.popup_close_button').forEach((closeButton) => {
+        closeButton.addEventListener('click', () => {
+            closeButton.parentNode.classList.add("hide_popup");
+            document.getElementById("blur_background").classList.remove("blur");
+        });
+    });
+}
+
+function importFromFileSystemButtonListener() {
+    document.getElementById("import_from_file_system_button").addEventListener("click", () => {
+        sendMessageToMain("open-file-system", "dir");
+    });
+}
+
+function importFromCSVButtonListener() {
+    document.getElementById("import_from_csv_button").addEventListener("click", () => {
+        sendMessageToMain("open-file-system", "csv");
+    });
 }

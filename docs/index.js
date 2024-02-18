149219084887fba43aefeb8d4a8e3aa87d3e0071
ISO53 <102249575+ISO53@users.noreply.github.com>
2024-02-18 23:53:11 +0300

    added support for mobile

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,7 @@
 
 // ************************ JS Starts ************************
 sliderListener();
+topNavbarListener();
 
 // ******************** Declare Functions ********************
 function sliderListener() {
@@ -14,11 +15,43 @@ function sliderListener() {
     function updateSlider(direction) {
         sliderPos += direction;
         slides.style.transform = `translateX(${100 * sliderPos}%)`;
-    
+
         leftSliderButton.classList.toggle("disabled", sliderPos === 0);
         rightSliderButton.classList.toggle("disabled", sliderPos === -slidesCount + 1);
-    };
-    
+    }
+
     leftSliderButton.onclick = () => updateSlider(1);
     rightSliderButton.onclick = () => updateSlider(-1);
+}
+
+function topNavbarListener() {
+    // When scrolling darken the background of top bar
+    const navbar = document.getElementById("top_bar");
+    window.onscroll = function () {
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            navbar.classList.add("darken");
+        } else {
+            navbar.classList.remove("darken");
+        }
+    };
+
+    // On mobile devices widen-up the top bar on the click of the button
+    const menuButton = document.getElementById("top_bar_open_menu_button");
+    menuButton.onclick = function () {
+        navbar.classList.toggle("open");
+    };
+
+    // On mobile devices close/narrow the top bar when clicking one of the links (href)
+    navbar.onclick = function (event) {
+        if (event.target.tagName.toLowerCase() === "a") {
+            navbar.classList.remove("open");
+        }
+    };
+
+    // On mobile devices if the clicked location outside of topbar, close/narrow the top bar
+    window.onclick = function (event) {
+        if (!event.target.closest("#top_bar")) {
+            navbar.classList.remove("open");
+        }
+    };
 }
